827277f97a1299799e706a29e956aead05390d06
yjyq1990 <695447272@qq.com>
2015-10-13 14:32:59 +0800

    add description and export header file

--- wtoeutil/CircularQueue.h
+++ wtoeutil/CircularQueue.h
@@ -7,24 +7,53 @@
 
 namespace wtoeutil 
 {
+    /**
+     * A cycle queue.
+     * CCircularQueue is a cycle queue of binary data,you can use it as a
+     * buffer
+     */
     class UTIL_EXPORT CCircularQueue
     {
     public:
         CCircularQueue();
-
         ~CCircularQueue();
     public:
+        /**
+         * Init the queue with @param len.
+         * Alloc the required len of the memory and init it
+         */
         bool init( uint32_t len );
 
+        /**
+         * Fini the queue and release the memory
+         */
         void fini();
 
+        /**
+         * Put data into queue
+         * @param buf is the data ptr you want to put into the queue
+         * @param len is the len of the data with byte
+         */
         int32_t pushdata( uint8_t *buf, uint32_t len );
 
         // 还是按len来读,但是不等len满,直接读完.
+        /**
+         * Get data from queue
+         * @param buf is the dest of the data you take from the queue
+         * @param len is the len of the data you want to get
+         * @retval 0 when success
+         * @retval <0 when error
+         */
         int32_t popdata( uint8_t *buf, uint32_t len );
 
+        /**
+         * Reset the queue
+         */
         void reset( bool stopwork );
 
+        /**
+         * Get the usable size
+         */
         uint32_t getsize();
     private:
         uint8_t *m_buf;
---
+++ wtoeutil/wtoelib.h
@@ -0,0 +1,7 @@
+#ifndef WTOELIB_H_
+#define WTOELIB_H_H
+
+#include "md5.h"
+#include "CircularQueue.h"
+
+#endif
