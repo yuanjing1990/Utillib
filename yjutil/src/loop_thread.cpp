d45d3b088c7b2bbdbeaa80523b5541247c75e9c4
yuanjing1990 <695447272@qq.com>
2021-10-28 21:29:38 +0800

    Update loop_thread.cpp

--- yjutil/src/loop_thread.cpp
+++ yjutil/src/loop_thread.cpp
@@ -1,12 +1,10 @@
 #include "loop_thread.h"
 #include <algorithm>
 
+#define LOG(format, ...) printf((format), ##__VA_ARGS__)
 loop_thread::~loop_thread() {
     if (!exitThreadPending()) {
         stop();
-        if (!isCurrentThread()) {
-            m_thread.join();
-        }
     }
 }
 
@@ -18,14 +16,18 @@ int loop_thread::start(std::string name) {
 
 void loop_thread::run() {
     bool ret = true;
-    while (!exitThreadPending() && ret) {
+    while (ret) {
         ret = threadLoop();
     }
 }
 
 int loop_thread::stop() {
+    LOG("Stop loop_thread!\n");
     requestThreadExit();
     m_eventListCond.notify_all();
+    if (m_thread.joinable() && !isCurrentThread()) {
+        m_thread.join();
+    }
     return 0;
 }
 
@@ -34,24 +36,33 @@ bool loop_thread::threadLoop() {
     {
         std::unique_lock<std::mutex> lc(m_eventListMutex);
         if (exitThreadPending()) {
+            LOG("Message count:%llu have not deal!\n", m_eventList.size());
             m_eventList.clear();
             return false;
         }
         if (m_eventList.empty()) {
+            LOG("Wait message to deal!\n");
             m_eventListCond.wait(lc);
+            LOG("Wait message to deal finish!\n");
             return true;
         }
         int64_t whenMs = m_eventList.front().whenMs;
-        int64_t nowMs = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now().time_since_epoch()).count();
+        int64_t nowMs = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now().time_since_epoch()).count();
         if (whenMs > nowMs) {
             int delayMs = whenMs - nowMs;
+            LOG("Wait message delay:%d to deal!\n", delayMs);
             m_eventListCond.wait_for(lc, std::chrono::milliseconds(delayMs));
+            LOG("Wait message delay:%d to deal finish!\n", delayMs);
             return true;
         }
         event = m_eventList.front();
         m_eventList.pop_front();
     }
-    if (m_handler != nullptr) {
+    if (!event.msg) {
+        LOG("Deal null message!\n");
+    } else if (event.msg->handler != nullptr) {
+        event.msg->handler(event.msg);
+    } else if (m_handler != nullptr) {
         m_handler(event.msg);
     }
     return true;
@@ -76,8 +87,10 @@ int loop_thread::cancelMessage(int what) {
     if (exitThreadPending() || m_eventList.empty()) {
         return -1;
     }
-    m_eventList.erase(std::remove_if(m_eventList.begin(), m_eventList.end(), [what](const auto &e) { return e.msg != nullptr && e.msg->what == what; }));
-    return 0;
+    auto it = std::remove_if(m_eventList.begin(), m_eventList.end(), [what](const auto &e) { return bool(e.msg) && e.msg->what == what; });
+    size_t sz = m_eventList.size();
+    m_eventList.erase(it, m_eventList.end());
+    return sz - m_eventList.size();
 }
 
 int loop_thread::postMessage(const std::shared_ptr<thread_msg> &msg, int delayMs) {
@@ -87,9 +100,9 @@ int loop_thread::postMessage(const std::shared_ptr<thread_msg> &msg, int delayMs
     }
     int64_t whenMs{0};
     if (delayMs > 0) {
-        whenMs = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now().time_since_epoch()).count() + delayMs;
+        whenMs = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now().time_since_epoch()).count() + delayMs;
     } else {
-        whenMs = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now().time_since_epoch()).count();
+        whenMs = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now().time_since_epoch()).count();
     }
     auto first = std::find_if(m_eventList.begin(), m_eventList.end(), [whenMs](const auto &e) { return e.whenMs > whenMs; });
     thread_event event{whenMs, msg};
